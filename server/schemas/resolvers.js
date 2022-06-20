630f82ac8d5d15fbc95955154131761e8a888598
Dave Oliverio <davezer27@gmail.com>
2022-06-20 16:05:41 -0400

    created server folder and built out schemas

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,102 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Trips } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+                    .populate('trips')
+                
+                return userData;
+            }
+            throw new AuthenticationError('Not logged in');
+        },
+
+        trips: async (parent, { email }) => {
+            const params = email ? { email } : {};
+            return Collection.find(params).sort({ createdAt: -1 });
+        },
+        trip: async (parent, { _id }) => {
+            return Collection.findOne({ _id });
+        },
+        user: async (parent, { email }) => {
+            return User.findOne({ email })
+                .select('-__v -password')
+                .populate('trips')
+        } 
+    },
+    Mutation: {
+        addUser: async (parent, args) => {
+            console.log(args);
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+        addTrip: async (parent, { tripName, category }, context) => {
+            if (context.user) {
+                const trip = await Trip.create({ tripName, category });
+
+                await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $push: { trips: trip._id } },
+                    { new: true }
+                );
+
+                return trip;
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        },
+        addItem: async (parent, { collectionId, itemName, description }, context) => {
+            if (context.user) {
+                const item = await Item.create({ itemName, description });
+                console.log(item);
+                
+                await Collection.findByIdAndUpdate(
+                    { _id: collectionId },
+                    { $push: { items: item._id } },
+                    { new: true, runValidators: true }
+                );
+                
+                return item;
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        },
+        
+        // Mutation to remove a trip from a User
+        removeCollection: async (parent, {  tripId }, context) => {
+            // return trip.findOneAndDelete({ _id: tripId });
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { trips: tripId } },
+                    { new: true }
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        },
+       
+    }
+};
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,38 @@
+const { gql } = require('apollo-server-express');
+
+// need to do some work on the mutations, not sure they are right
+const typeDefs = gql`
+    type Auth {
+        token: ID!
+        user: User
+    }
+    type User {
+        _id: ID
+        email: String
+        tripCount: Int
+        trips: [Trips]
+    }
+    type Trip {
+        _id: ID
+        tripName: String
+        category: String
+        email: String
+       
+    }
+    type Query {
+        me: User
+        users: [User]
+        user(email: String!): User
+        trips(email: String): [Trip]
+        trip(_id: ID!): Trip
+    
+    }
+    
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(email: String!, password: String!): Auth
+        addTrip(tripName: String!, category: String!): Trip
+        removeTrip(TripId: ID!): User
+      
+    }
+`;
